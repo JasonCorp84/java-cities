bc4a9871265866bac4606d2da44beb2202dda2df
JasonCorp84 <csababalogh84@gmail.com>
2019-02-13 13:04:36 -0800

    project initiated

---
+++ src/main/java/com/lambdaschoo/cities/restcities/RestcitiesApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschoo.cities.restcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestcitiesApplication.class, args);
+    }
+
+}
+
